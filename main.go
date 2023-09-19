bd7e48f39d1d89ec4a8572ee8ae7a88870e6b5af
jon brookes <marshyon@gmail.com>
2023-09-19 18:17:57 +0100

    refactored into funcs

--- main.go
+++ main.go
@@ -37,34 +37,42 @@ type ConvertedBookmark struct {
 	Tags        string    `json:"tags"`
 }
 
-func main() {
-	// Open an SQLite database
+func initDB() (*gorm.DB, error) {
 	db, err := gorm.Open(sqlite.Open("bookmarks.db"), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Error opening database:", err)
-		return
+		return nil, err
 	}
+	fmt.Println("Database connection successfully opened.")
+	return db, nil
+}
 
-	// Migrate the schema
-	db.AutoMigrate(&ConvertedBookmark{})
-
-	// Read the JSON file
+// function to read the json file
+func readJSONFile() ([]byte, error) {
 	data, err := os.ReadFile("bookmarks.json")
 	if err != nil {
 		fmt.Println("Error reading file:", err)
-		return
+		return nil, err
 	}
+	return data, nil
+}
 
-	// Create a slice to hold the original bookmarks
+// function to
+// Create a slice to hold the original bookmarks
+// Unmarshal the JSON data into the original bookmarks slice
+func unmarshalJSON(data []byte) ([]OriginalBookmark, error) {
 	var originalBookmarks []OriginalBookmark
-
-	// Unmarshal the JSON data into the original bookmarks slice
 	if err := json.Unmarshal(data, &originalBookmarks); err != nil {
 		fmt.Println("Error parsing JSON:", err)
-		return
+		return nil, err
 	}
+	return originalBookmarks, nil
+}
 
-	// Iterate over the original bookmarks and convert them
+// function to
+// Iterate over the original bookmarks and convert them
+func convertBookmarks(originalBookmarks []OriginalBookmark) ([]ConvertedBookmark, error) {
+	var convertedBookmarks []ConvertedBookmark
 	for _, bookmark := range originalBookmarks {
 		// Parse the time string into a time.Time object
 		t, err := time.Parse(time.RFC3339, bookmark.Time)
@@ -85,20 +93,37 @@ func main() {
 			ToRead:      bookmark.ToRead,
 			Tags:        bookmark.Tags,
 		}
+		convertedBookmarks = append(convertedBookmarks, convertedBookmark)
+	}
+	return convertedBookmarks, nil
+}
 
+// function to upsert the converted bookmarks and output count ever other second
+func upsertBookmarks(db *gorm.DB, convertedBookmarks []ConvertedBookmark) error {
+	count := 0
+	total := len(convertedBookmarks)
+	for _, convertedBookmark := range convertedBookmarks {
+		count++
+		if count%500 == 0 {
+			fmt.Printf("Upserted %d bookmarks :: ", count)
+			percentages := float64(count) / float64(total) * 100
+			fmt.Printf("[%.0f%%]\n", percentages)
+		}
 		// Perform upsert operation
 		if err := db.Clauses(clause.OnConflict{
 			Columns:   []clause.Column{{Name: "hash"}},
 			UpdateAll: true,
 		}).Create(&convertedBookmark).Error; err != nil {
 			fmt.Println("Error upserting bookmark:", err)
+			return err
 		}
 	}
+	return nil
+}
 
-	// Now, you have the data upserted in the "bookmarks.db" SQLite database
-
-	// Iterate over the converted bookmarks and print their contents
-	for _, bookmark := range originalBookmarks {
+// print out the converted bookmarks
+func printConvertedBookmarks(convertedBookmarks []ConvertedBookmark) {
+	for _, bookmark := range convertedBookmarks {
 		fmt.Printf("Href: %s\n", bookmark.Href)
 		fmt.Printf("Description: %s\n", bookmark.Description)
 		fmt.Printf("Extended: %s\n", bookmark.Extended)
@@ -109,5 +134,51 @@ func main() {
 		fmt.Printf("ToRead: %s\n", bookmark.ToRead)
 		fmt.Printf("Tags: %s\n", bookmark.Tags)
 		fmt.Println("---------------------------------------------")
+	}
+}
+
+func main() {
+
+	// read command line arguments and check to see if verbose is set
+	// if verbose is set then print out the converted bookmarks
+	verbose := false
+	if len(os.Args) > 1 {
+		if os.Args[1] == "-v" {
+			verbose = true
+		}
+	}
+
+	db, err := initDB()
+	if err != nil {
+		fmt.Println("Error opening database:", err)
+		return
+	}
+	// defer db.Close()
+
+	db.AutoMigrate(&ConvertedBookmark{})
+
+	data, err := readJSONFile()
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	originalBookmarks, err := unmarshalJSON(data)
+	if err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		return
+	}
+
+	ConvertedBookmarks, err := convertBookmarks(originalBookmarks)
+	upsertBookmarks(db, ConvertedBookmarks)
+	if err != nil {
+		fmt.Println("Error converting bookmarks:", err)
+		return
+	}
+
+	total := len(ConvertedBookmarks)
+	fmt.Printf("Total bookmarks: %d\n", total)
+	if verbose {
+		printConvertedBookmarks(ConvertedBookmarks)
 	}
 }
